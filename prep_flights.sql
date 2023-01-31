4fdf0af59258236ec1c40dfc8b85be5e41b42656
Andrey <123981558+mrafalcon@users.noreply.github.com>
2023-01-31 16:04:16 +0300

    Update prep_flights.sql

--- prep_flights.sql
+++ prep_flights.sql
@@ -1,5 +1,7 @@
 delete from project1.query_passangers;
 
+EXPLAIN (ANALYSE, VERBOSE) --план запроса
+
 with 
 PrepTable as (
 	select 
@@ -25,7 +27,7 @@ select id_route, id_naryad, id_car, id_stop, time_dep,
 
 from PrepTable
 where to_depot=0 and count_flights is not null
-order by date_time, id_route, id_naryad, id_part, time_dep
+--order by date_time, id_route, id_naryad, id_part, time_dep
 	),
 PrepTable3 as (
 	select *
@@ -48,13 +50,15 @@ PrepTable6 as (
 select *
 from PrepTable5
 where next_time_dep - time_dep < '04:00:00'
-order by time_dep, id_route, id_naryad, id_car),
+--order by time_dep, id_route, id_naryad, id_car
+),
 PrepTable7 as (	
-select p6.id_route, p6.id_naryad, p6.id_car, case when p6.id_stop = r.stop_1 then r.stop_1 else r.stop_2 end as id_stop, p6.time_dep, p6.next_time_dep
+select p6.id_route, p6.id_naryad, p6.id_car, case when p6.id_stop = r.stop_1 then r.stop_1 else r.stop_2 end as id_stop, 
+    p6.time_dep, p6.next_time_dep
 from PrepTable6 p6
 join project1.routes r
 on p6.id_route = r.id_route
-order by p6.time_dep, p6.id_route, p6.id_naryad
+--order by p6.time_dep, p6.id_route, p6.id_naryad
 	),
 
 PrepTable8 as (
@@ -68,7 +72,7 @@ from PrepTable8 p8
 join project1.cards pc
 on p8.id_route = pc.id_route and p8.id_naryad = pc.id_naryad and p8.id_car = pc.id_car and p8.time_dep < pc.date_time and pc.date_time < p8.next_time_dep
 where pc.id_error is null
-order by p8.time_dep, p8.id_route, p8.id_naryad, pc.date_time
+--order by p8.time_dep, p8.id_route, p8.id_naryad, pc.date_time
 ),
 
 passangers as (
@@ -113,15 +117,10 @@ select *, next_created_at-created_at as duration,
 		when (flight is null or next_flight is null) then 6 -- транзакции на парковых рейсах
 		when created_at::time < '04:00:00' then 5 -- транзакция до начала движения по маршрутам
 		when car = next_car and (route = next_route and id_naryad = passangers.next_id_naryad and (flight = next_flight or row_flight = next_row_flight)) then 4 -- повторное прикладывание на 1 рейсе
-		--when car = next_car and next_created_at-created_at < '02:00:00' then 4 -- повторное прикладывание на 1 рейсе
-		--when (next_created_at-created_at > '16:00:00' ) then 3 -- промежуток более 16 часов (при нормальном 8часовом сне для человека)	
-		--when (next_created_at-created_at < '00:01:30' and car = next_car) then 2 --повторное прикладывание в пределах 1 перегона?
-		--when (route = '19 ТМ' and next_created_at-created_at < '00:17:30' and car = next_car) then 1 --повторное прикладывание в пределах 1 рейса? 19 ТМ - 1 рейс -> 18мин30сек - перегон без пассажиров (1 минута) = 17:30
-		--when (route = '48 ТМ' and next_created_at-created_at < '00:45:30' and car = next_car) then 1 --повторное прикладывание в пределах 1 рейса? 48 ТМ 1 рейс - перегон без пассажиров (1 минута)
 	end as possible_error
 from passangers
 where next_created_at:: date = created_at:: date and mod (row_number, 2) = 1
-order by next_created_at-created_at ASC 
+--order by next_created_at-created_at ASC 
 ) n
-where possible_error is null
-order by time_dep
+where possible_error is null;
+--order by time_dep
